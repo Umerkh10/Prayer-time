53bc98fa731b738603c4d6a720cce7f1c0e106d9
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-03-24 16:39:38 +0500

    Add AuthenticatedRoute component to protect admin pages; refactor AdminResponsivePage to use it

---
+++ src/components/AuthenticatedRoute.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { urlSplitter } from "@/lib";
+
+const AuthenticatedRoute = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const lang = urlSplitter(pathname);
+
+  useEffect(() => {
+    const user = localStorage.getItem("userData");
+    const parsedUser = user ? JSON.parse(user) : null;
+
+    if (parsedUser?.role !== "admin") {
+      router.push(`/${lang}/forum`);
+    }
+  }, [router, pathname]);
+
+  return <>{children}</>;
+};
+
+export default AuthenticatedRoute;
